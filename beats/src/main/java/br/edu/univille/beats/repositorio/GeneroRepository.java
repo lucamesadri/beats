ec3f938b68ee84842b096b30fbf51a54bbf6d09d
Luca Vinícius Mesadri <lucavmesadri@gmail.com>
2024-11-07 20:40:31 -0300

    Deleter codigo antigo

--- beats/src/main/java/br/edu/univille/beats/repositorio/GeneroRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.univille.beats.repositorio;
-
-import br.edu.univille.beats.entity.Genero;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GeneroRepository extends JpaRepository<Genero, Long> {
-    Genero findByNome(String nome);
-}
